191d75d4f40f3791a5d30f713050aab343c1f6e9
mahmoudmortada <mortda.m.11@gmail.com>
2018-07-03 21:34:08 +0200

    fix: remove unused callback function from MemEditorCiewController.swift

--- memeMe/MemeEditorViewController.swift
+++ memeMe/MemeEditorViewController.swift
@@ -50,11 +50,7 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
       navigationController?.popViewController(animated: true)
         
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+
     
     @IBAction func shareMeme(_ sender: Any) {
 
